c7a43c0c48383aa3235cbcf8c695b7d998e3ca4b
Melissa Ben <melissabenmeziane01@gmail.com>
2021-11-03 11:18:35 +0100

    annd function Create

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Crud;
+use App\Form\CrudType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -16,5 +19,26 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    /**
+     * @Route("create", name="create")
+     */
+    public function create(Request $request)
+    {
+       $crud = new Crud();
+       $form = $this->createForm(CrudType::class , $crud);
+       $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($crud);
+            $em->flush();
+
+            $this->addFlash('notice','Submitted successfully!');
+        }
+       return $this->render('main/create.html.twig',[
+           'form' => $form -> createView()
+       ]);
     }
 }
